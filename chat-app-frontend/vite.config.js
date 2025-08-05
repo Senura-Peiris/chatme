29736a3e263e51196eeb0d7da23ea292f419727a
Senura Peiris <senurapieris@gmail.com>
2025-08-05 16:42:27 +0530

    make some changes

--- chat-app-frontend/vite.config.js
+++ chat-app-frontend/vite.config.js
@@ -4,10 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(),
-           tailwindcss()],
-  server: {
-    proxy: {
-      '/api': 'https://chatme-production-6ae4.up.railway.app',
-    },
+    tailwindcss()],
+  build: {
+    outDir: 'dist',
   },
+  base: './', // Ensure relative paths for Netlify
 });
